2773fafe2ca9f6049614e5b4109d66a4a50774c0
Navaneeswar0213 <78856690+Navaneeswar0213@users.noreply.github.com>
2021-02-13 22:36:40 +0530

    Add files via upload

---
+++ Hello-World-Spring-boot-Project/src/main/java/springboot/project/HelloWorldSpringbootProject/HelloWorldSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package springboot.project.HelloWorldSpringbootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ Hello-World-Spring-boot-Project/src/test/java/springboot/project/HelloWorldSpringbootProject/HelloWorldSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.project.HelloWorldSpringbootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
